23b48d449823de7bbd3bc756ca0abde5550a58d2
German Levental <gerleven@gmail.com>
2024-01-21 17:46:47 -0300

    root page content, create css class for first mobile

--- src/routes/root-page.tsx
+++ src/routes/root-page.tsx
@@ -3,7 +3,26 @@ import "../App.css";
 import { Stack } from "@mui/material";
 
 const RootPage = () => {
-  return <></>;
+  return (
+    <>
+      <Stack
+        className="fullscreen"
+        direction={"column"}
+        justifyContent={"start"}
+        alignItems={"center"}
+      >
+        <Stack
+          className="app-wrapper"
+          direction={"column"}
+          justifyContent={"start"}
+          alignItems={"strech"}
+        >
+          <div>Banner!</div>
+          <Outlet />
+        </Stack>
+      </Stack>
+    </>
+  );
 };
 
 export default RootPage;
