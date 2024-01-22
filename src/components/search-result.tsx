f91312a938da911a29039dec8350bb679e81681d
German Levental <gerleven@gmail.com>
2024-01-22 14:30:59 -0300

    create a card for notify no items in the list

--- src/components/search-result.tsx
+++ src/components/search-result.tsx
@@ -1,8 +1,14 @@
-import { Stack } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import { Character } from "../lib/definitions.tsx";
 import CharacterCard from "./character-card";
+import { CustomButtonPrimary } from "../lib/utils.tsx";
+import theme from "../theme/custom-theme.tsx";
 
-export default function SearchResult({ characters }: any) {
+export default function SearchResult({
+  characters,
+}: {
+  characters: Character[];
+}) {
   return (
     <>
       <Stack
@@ -13,10 +19,31 @@ export default function SearchResult({ characters }: any) {
         padding={1}
         className={"overFlowYScroll"}
       >
-        {characters.map((character: Character, index: number) => {
-          return <CharacterCard key={index} character={character} />;
-        })}
+        {characters.length == 0 && <NoItemsToShow />}
+        {characters.length != 0 && <ListOfItems characters={characters} />}
       </Stack>
     </>
   );
 }
+
+const NoItemsToShow = () => {
+  return (
+    <>
+      <Paper elevation={3}>
+        <Box>
+          <Stack padding={2} spacing={1}>
+            <Typography fontSize={20} fontWeight={500} >No Items to show</Typography>
+            <Typography fontWeight={300} color={theme.palette.grey[600]}>Try to create a new Character or call API again</Typography>
+            <CustomButtonPrimary>Call API</CustomButtonPrimary>
+          </Stack>
+        </Box>
+      </Paper>
+    </>
+  );
+};
+
+const ListOfItems = ({ characters }: { characters: Character[] }) => {
+  return characters.map((character: Character, index: number) => {
+    return <CharacterCard key={index} character={character} />;
+  });
+};
