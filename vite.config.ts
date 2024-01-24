08d86faeae3a057bf527dfefce0870f9b331cfc3
German Levental <gerleven@gmail.com>
2024-01-24 19:15:40 -0300

    clean code in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  // Load env file based on `mode` in the current working directory.
-
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(({ command }) => {
+  
   return {
     // dev specific config
     plugins: [react()],
-    base: "./",
-    define: {
-      BASENAME: JSON.stringify(command === "serve" ? "/" : "/StarWars-Characters"),
-    },
+    base: "./"
   };
 });
