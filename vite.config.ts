1cbcbf247c8fc97ed0fc6b5528b1525c4bc66cab
German Levental <gerleven@gmail.com>
2024-01-24 19:17:34 -0300

    clean code in vite.config v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  
-  return {
-    // dev specific config
-    plugins: [react()],
-    base: "./"
-  };
+export default defineConfig({
+  // dev specific config
+  plugins: [react()],
+  base: "./",
 });
